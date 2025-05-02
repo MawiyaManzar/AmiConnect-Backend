3e28298388160d2247720b0536a4356ba11824d0
MawiyaManzar <mawiyamanzar28@gmail.com>
2025-05-02 12:13:17 +0530

    edited file in app.py

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ load_dotenv()
 DB_URL = os.getenv('DATABASE_URL')
 SECRET_KEY = os.getenv('SECRET_KEY')
 app = Flask(__name__)
+app.config['ENV'] = os.getenv('FLASK_ENV', 'production')
 CORS(app)
 
 app.register_blueprint(auth_bp)
@@ -28,6 +29,5 @@ def index():
     return {'status': 'AmiConnect backend running'}
 
 if __name__ == '__main__':
-    print(app.url_map)
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
