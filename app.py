0b51570a796607e7876ae57dddefa81836ff03ed
MawiyaManzar <mawiyamanzar28@gmail.com>
2025-05-05 02:26:54 +0530

    changed port to 10000

--- app.py
+++ app.py
@@ -39,5 +39,5 @@ def index():
     return {'status': 'AmiConnect backend running'}
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8000))
+    port = int(os.environ.get('PORT', 10000))
     app.run(host='0.0.0.0', port=port, debug=True)
